cdb7598174974cadf38da04f85aaa23f4f360036
Alek <alekaker@outlook.com>
2019-07-13 13:43:47 -0600

    add deck class and pass first test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,16 +1,24 @@
 import chai from 'chai';
 const expect = chai.expect;
-import Round from '..src/round'
+import Deck from '../src/Deck';
 
 describe('Deck', function() {
+
+  let deck1;
+  beforeEach(function() {
+    deck1 = new Deck
+  });
+
 
   it('should be a function', function() {
     expect(Deck).to.be.a('function');
   });
 
   it('should be an instance of Game', function() {
-    expect(deck).to.be.an.instanceof(Deck);
+    expect(deck1).to.be.an.instanceof(Deck);
   });
+
+  
 
 
 });
