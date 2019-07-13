34cfa782fbd4dc14c601a583de24fd85ffdc521b
Alek <alekaker@outlook.com>
2019-07-13 14:03:53 -0600

    add cards and categories properties to deck

--- src/Deck.js
+++ src/Deck.js
@@ -1,6 +1,12 @@
 class Deck {
-  constructor () {
+  constructor (data) {
+    this.categories = data.categories
+    this.cards = data.clues
+  }
 
+  returnNewData() {
+
+    return this.newDataSet
   }
 }
 
--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,12 +1,13 @@
 import chai from 'chai';
 const expect = chai.expect;
+import data from '../src/dataset.js'
 import Deck from '../src/Deck';
 
 describe('Deck', function() {
 
   let deck1;
   beforeEach(function() {
-    deck1 = new Deck
+    deck1 = new Deck(data)
   });
 
 
@@ -18,7 +19,29 @@ describe('Deck', function() {
     expect(deck1).to.be.an.instanceof(Deck);
   });
 
-  
+  it('should hold data', function() {
+    expect(deck1.categories).to.eql({
+        'unitedStatesHistory': 1,
+        'lifeSciences': 2,
+        'publicHealth': 3,
+        'educationJargon': 4,
+        'nameThatBoardGame': 5,
+        'americanLiterature': 6,
+        'biographies': 7,
+        'americanCities': 8,
+        'food': 9,
+        'cableTV': 10
+      })
+  });
+
+  it('should have a cards property and hold cards', function() {
+      expect(deck1.cards.length).to.eql(114)
+  });
+
+
+
+
+
 
 
 });
