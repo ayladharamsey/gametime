ef7aa023ff36d5def2390451e430451da7630e1a
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-07-14 11:52:14 -0600

    Complete minimum value, but maximum value needs more work

--- test/Player-test.js
+++ test/Player-test.js
@@ -15,5 +15,16 @@ describe('player', () => {
     expect(Player).to.be.a('function');
   });
 
-  it.only()
-});
+  it.only("should be an instance of a Player", () => {
+    expect(player).to.be.a.instanceOf(Player);
+  });
+
+  it.skip("should have a minimium value of 5 to wage", () => {
+    expect(player.playerWager).to.be.at.least(5);
+  });
+
+  it.skip("should have a maximum value of the highest point value on the board or the current player's total score", () => {
+    expect(player.playerWager).to.be.at.below(player.playerScore || pointValue);
+  });
+
+}); 
