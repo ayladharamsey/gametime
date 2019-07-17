3e82e0e7ed2bf9681396d940a8032d6a8d257db7
Alek <alekaker@outlook.com>
2019-07-17 15:00:28 -0600

    add instantiation of game to fetch.then

--- src/index.js
+++ src/index.js
@@ -3,54 +3,42 @@ import $ from 'jquery';
 import Game from '../src/Game.js'
 
 var data
+var game
 fetch("https://fe-apps.herokuapp.com/api/v1/gametime/1903/jeopardy/data")
 .then(response => response.json())
 .then(fetchData => data = fetchData.data)
-
-$(document).ready(() => {
-  setTimeout(function() {
-    let game = new Game(data)
-    game.startRound()
-    let round = game.round
-    round.categories.forEach( (cat, index) => {
-      $(`#category-name-${index + 1}`).html(cat[0])
-    })
-    round.cardSet.forEach(el => el.forEach( (card, index) => {
-      $(`#category-${index + 1}-${card.pointValue.toString()}`).html(card.question)
-    }))
-
-    var answer
-    $(document).on( "click", function(e) {
-      var question = $( e.target ).closest( "th" ).text()
-      round.cardSet.forEach(array1 => array1.find(el => {
-        if (el.question === question) {
-          answer = el.answer
-          console.log(answer)
-        }
-
-      }))
+.then(data => game = new Game(data))
 
 
-    });
-  }, 500);
 
-});
+setTimeout(() => {
+game.startRound()
+makeBoard(game.round)
+getAnswers(game.round)
+}
+,500)
 
-  //DOM.js event methods
-  // playerPickCategory() {
-  // }
-  // playerPickCardValue(valuePicked) {
-  // }
-  //combine with evaluate player guess
-  // changePlayerScore() {
 
-  // }
-  // evaluatePlayersGuesses(playerGuess, correctAnswer) {
 
-  // }
-//   determineCurrentPlayer() {
-//   }
 
-//   determinePlayersEndOfTurn() {
-//   }
-// }
+var answer
+function getAnswers(round) {
+  $(document).on( "click", function(e) {
+    var question = $( e.target ).closest( "th" ).text()
+    round.cardSet.forEach(array1 => array1.find(el => {
+      if (el.question === question) {
+        answer = el.answer
+        console.log(answer)
+      }
+    }));
+  });
+}
+
+function makeBoard(currentRound) {
+  currentRound.categories.forEach( (cat, index) => {
+    $(`#category-name-${index + 1}`).html(cat[0])
+  })
+  currentRound.cardSet.forEach(el => el.forEach( (card, index) => {
+    $(`#category-${index + 1}-${card.pointValue.toString()}`).html(card.question)
+  }))
+}
