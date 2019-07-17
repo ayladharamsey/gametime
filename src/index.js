081fc6f808793a4329012330a0b3d4387bebe4b1
Alek <alekaker@outlook.com>
2019-07-16 18:00:31 -0600

    add event targeting for questions

--- src/index.js
+++ src/index.js
@@ -6,16 +6,31 @@ fetch("https://fe-apps.herokuapp.com/api/v1/gametime/1903/jeopardy/data")
 .then(fetchData => data = fetchData.data)
 
 $(document).ready(() => {
-  setTimeout(popBoard, 500)
-});
+  setTimeout(function() {
 
-function popBoard() {
-  let deck1 = new Deck(data)
-  deck1.assignCategoriesRound1()
-  deck1.roundCategories.forEach( (cat, index) => {
-  $(`#category-name-${index + 1}`).html(cat[0])
-  })
-  deck1.cardSet.forEach(el => el.forEach( (card, index) => {
-  $(`#category-${index + 1}-${card.pointValue.toString()}`).html(card.question)
-  }))
-}
+    // $('.card').removeAttr('')
+    var deck1 = new Deck(data)
+    deck1.assignCategoriesRound1()
+    deck1.roundCategories.forEach( (cat, index) => {
+      $(`#category-name-${index + 1}`).html(cat[0])
+    })
+    deck1.cardSet.forEach(el => el.forEach( (card, index) => {
+      $(`#category-${index + 1}-${card.pointValue.toString()}`).html(card.question)
+    }))
+
+    var answer
+    $(document).on( "click", function(e) {
+      var question = $( e.target ).closest( "th" ).text()
+      deck1.cardSet.forEach(array1 => array1.find(el => {
+        if (el.question === question) {
+          answer = el.answer
+          console.log(answer)
+        }
+
+      }))
+
+
+    });
+  }, 500);
+
+});
