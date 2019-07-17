c75f537208c485842750ed8cca6d320086ac8e07
Emily <lalonde0902@gmail.com>
2019-07-17 14:10:48 -0600

    Remove excess html pages and consolidated them in 1

--- src/index.js
+++ src/index.js
@@ -6,35 +6,42 @@ fetch("https://fe-apps.herokuapp.com/api/v1/gametime/1903/jeopardy/data")
 .then(response => response.json())
 .then(fetchData => data = fetchData.data)
 
-$(document).ready(() => {
-  setTimeout(function() {
+
+$('.start-game-button').on('click', function(e) {
+  e.preventDefault();
+  $('.splash-page').hide();
+  $('.main-page').show()
+});
+
+// $(document).ready(() => {
+  // setTimeout(function() {
 
     // $('.card').removeAttr('')
-    var game = new Game(data)
-    game.assignCategoriesRound1()
-    game.roundCategories.forEach( (cat, index) => {
-      $(`#category-name-${index + 1}`).html(cat[0])
-    })
-    game.cardSet.forEach(el => el.forEach( (card, index) => {
-      $(`#category-${index + 1}-${card.pointValue.toString()}`).html(card.question)
-    }))
+//     var game = new Game(data)
+//     game.assignCategoriesRound1()
+//     game.roundCategories.forEach( (cat, index) => {
+//       $(`#category-name-${index + 1}`).html(cat[0])
+//     })
+//     game.cardSet.forEach(el => el.forEach( (card, index) => {
+//       $(`#category-${index + 1}-${card.pointValue.toString()}`).html(card.question)
+//     }))
 
-    var answer
-    $(document).on( "click", function(e) {
-      var question = $( e.target ).closest( "th" ).text()
-      game.cardSet.forEach(array1 => array1.find(el => {
-        if (el.question === question) {
-          answer = el.answer
-          console.log(answer)
-        }
+//     var answer
+//     $(document).on( "click", function(e) {
+//       var question = $( e.target ).closest( "th" ).text()
+//       game.cardSet.forEach(array1 => array1.find(el => {
+//         if (el.question === question) {
+//           answer = el.answer
+//           console.log(answer)
+//         }
 
-      }))
+//       }))
 
 
-    });
-  }, 500);
+//     });
+//   }, 500);
 
-});
+// });
 
   //DOM.js event methods
   // playerPickCategory() {
