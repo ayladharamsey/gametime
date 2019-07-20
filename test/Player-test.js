e2b95fd0c8038b84a2902fbc8354cfd121d1a529
Ann Cerveny <cervann11@gmail.com>
2019-07-19 19:31:20 -0600

    Update Round class to include roundCounter and dailyDouble2 method

--- test/Player-test.js
+++ test/Player-test.js
@@ -2,7 +2,7 @@ import chai from 'chai'
 import Player from '../src/Player.js'
 const expect = chai.expect;
 
-describe('player', () => {
+describe('Player', () => {
   let player;
 
   beforeEach(() => {
@@ -17,13 +17,13 @@ describe('player', () => {
     expect(player).to.be.a.instanceOf(Player);
   });
 
-  it("should have a minimium value of 5 to wage", () => {
-    player.manageWagers();
-    expect(player.canWager).to.equal(true);
-  });
+  // it("should have a minimium value of 5 to wage", () => {
+  //   player.manageWagers();
+  //   expect(player.canWager).to.equal(true);
+  // });
 
-  it.skip("should have a maximum value of the highest point value on the board or the current player's total score", () => {
-    expect(player.playerWager).to.be.at.below(player.playerScore || round.cards.pointValue);
-  });
+  // it.skip("should have a maximum value of the highest point value on the board or the current player's total score", () => {
+  //   expect(player.playerWager).to.be.at.below(player.playerScore || round.cards.pointValue);
+  // });
 
 }); 
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,16 +1,20 @@
 import chai from 'chai'
+import data from '../src/dataset.js'
 import Player from '../src/Player.js'
 import Round from '../src/Round.js'
+import Game from '../src/Game.js';
 const expect = chai.expect;
 
 describe('Round', () => {
   let round; 
   let player;
+  let game;
 
   beforeEach(() => {
-    player = new Player('bill')
-    round = new Round(game);
-    // game.assignCategoriesRound1();
+    game = new Game(data)
+    game.startRound()
+    // player = new Player('bill')
+    round = new Round(game)
   });
 
   it('should be a function', () => {
@@ -21,11 +25,40 @@ describe('Round', () => {
     expect(round).to.be.an.instanceOf(Round);
   }); 
 
-  it.skip('should return remove clicked clues from array', () => {
-    expect(round.removeClickedCategoriesFromArray()).to.equal([[1, 1, 1, 1], [2, 2, 2], [3, 3], [4]]);
+  it('should be instantiated with a game object argument', () => {
+    expect(round.game).to.not.equal(undefined);
+  }); 
+  
+
+  it('should generate a random dailyDouble1 value', () => {
+    expect(round.dailyDouble1).to.not.equal(undefined);
+  }); 
+
+  it('should have dailyDouble2 as initially undefined', () => {
+    expect(round.dailyDouble2).to.equal(undefined);
+  }); 
+
+  it('should receive the categories selected for the round per Game', () => {
+    expect(round.categories.length).to.eql(4);
+  }); 
+
+  it('should generate a second dailyDouble if the currentRoundNum is 2', () => {
+    game.startRound()
+    round = new Round(game)
+    expect(round.generateRandomDailyDoubleCard2()).to.not.equal(undefined);
+  }); 
+
+
+
+  it('should have receive a cardSet from Game with a length of 4 (with 4 items in each)', () => {
+    expect(round.cardSet.length).to.eql(4)
+    expect(round.cardSet[0].length).to.eql(4)
+    expect(round.cardSet[1].length).to.eql(4)
+    expect(round.cardSet[2].length).to.eql(4)
+    expect(round.cardSet[3].length).to.eql(4)
   });
 
-  it.skip('should end game once array length is 0', () => {
-    expect(round.endGame()).to.equal();
-  });
+  // it.skip('should end game once array length is 0', () => {
+  //   expect(round.endGame()).to.equal();
+  // });
 });
