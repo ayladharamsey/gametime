6204b149cce6a1b3be179b7baea01e8d598bbbfb
Emily Lalonde <lalonde0902@gmail.com>
2019-07-20 11:39:10 -0600

    Revert "Inputs on splash-page disabled until inputs filled"

--- src/Game.js
+++ src/Game.js
@@ -19,10 +19,10 @@ class Game {
     this.currentRoundNum++
     if (this.currentRoundNum === 1) {
       this.assignCategoriesRound1()
-      this.currentRound = new Round(this.roundCategories, this.cardSet)
+        this.currentRound = new Round(this.roundCategories, this.cardSet)
     } else if (this.currentRound === 2) {
       this.assignCategoriesRound2()
-      this.currentRound = new Round(this.roundCategories, this.cardSet)
+        this.currentRound = new Round(this.roundCategories, this.cardSet)
     }
     // work on round 3 - diff behavior
   }
